b4b3891ad5a39796809bbaf92dcf67652e882596
seyitemin şanver <seyiteminsanver@gmail.com>
2022-12-29 07:22:24 +0300

    29/12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoryComponent } from './componants/category/category.component';
 import { ProductComponent } from './componants/product/product.component';
 import { NaviComponent } from './componants/navi/navi.component';
+import { VatAddedPipe } from './pipes/vat-added.pipe';
+import { FilterPipePipe } from './pipes/filter-pipe.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CategoryComponent,
     ProductComponent,
-    NaviComponent
+    NaviComponent,
+    VatAddedPipe,
+    FilterPipePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
